2ef20b79b1de9634e5d4d403170013dc5aae6cdf
Vasiliy Yurkovets <vyurkovets@int.codinsula.com>
2019-04-25 11:07:46 +0300

    Updates

---
+++ precache-manifest.369ceea262f915d420995479027a273c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6547ed043cbb9374a2d26a82fd9026c",
+    "url": "/goit-react-hw-02/index.html"
+  },
+  {
+    "revision": "991856be0048a09999dc",
+    "url": "/goit-react-hw-02/static/css/main.889e6209.chunk.css"
+  },
+  {
+    "revision": "2bfad6b8ee3671302fe5",
+    "url": "/goit-react-hw-02/static/js/2.ff657fb7.chunk.js"
+  },
+  {
+    "revision": "991856be0048a09999dc",
+    "url": "/goit-react-hw-02/static/js/main.8df47eb6.chunk.js"
+  },
+  {
+    "revision": "d92f35f1efebc38103b8",
+    "url": "/goit-react-hw-02/static/js/runtime~main.577bbb3b.js"
+  }
+]);
